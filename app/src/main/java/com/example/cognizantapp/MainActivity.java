cb855f2d2e5bea6c962b4305a23d6d31868bde99
abdul ansari <abdul.tanveer@gmail.com>
2022-07-06 00:31:48 +0530

    layouts and getting handle on edittexts

--- app/src/main/java/com/example/cognizantapp/MainActivity.java
+++ app/src/main/java/com/example/cognizantapp/MainActivity.java
@@ -4,18 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText nameEditText; //declaration
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameEditText = findViewById(R.id.etName); //initialization
     }
 
     public void clickHandler(View view) {
-        Toast.makeText(this, "loggin in", Toast.LENGTH_SHORT).show();
+        String name = nameEditText.getText().toString();
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
 
     }
 }
