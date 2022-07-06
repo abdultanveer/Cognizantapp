48446958d779fce79d9a586c46adacded8b010f8
abdul ansari <abdul.tanveer@gmail.com>
2022-07-07 00:34:05 +0530

    lifecycle of an activity

--- app/src/main/java/com/example/cognizantapp/MainActivity.java
+++ app/src/main/java/com/example/cognizantapp/MainActivity.java
@@ -6,15 +6,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public  static String TAG = MainActivity.class.getSimpleName();
     EditText nameEditText; //declaration
     TextView resultTv;
     int a;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +25,42 @@ public class MainActivity extends AppCompatActivity {
         nameEditText = findViewById(R.id.etName); //initialization
         resultTv = findViewById(R.id.tvResult);
         a = 10;
+        Log.i(TAG,"im in onCreate");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"im in onstart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w(TAG,"im in onResume --restore the app state");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG,"im in onPause-- save app state");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"im in onStop");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG,"im in onDestroy");
+
     }
 
     public void clickHandler(View clickedView) {
