4f949bcba2c21f678f90084b20b9a932a92e44a6
abdul ansari <abdul.tanveer@gmail.com>
2022-07-11 19:31:51 +0530

    recyclerview part -3

--- app/src/main/java/com/example/cognizantapp/DishAdapter.java
+++ app/src/main/java/com/example/cognizantapp/DishAdapter.java
@@ -1,34 +1,54 @@
 package com.example.cognizantapp;
 
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 //holder = reserve
 //DishAdapter.DishViewHolder = inner class
 public class DishAdapter extends RecyclerView.Adapter<DishAdapter.DishViewHolder> {
+    public static String TAG = DishAdapter.class.getSimpleName();
 
+    String[] dishes;
+
+    public DishAdapter(String[] mDishes){
+        dishes = mDishes;
+    }
 
     @NonNull
     @Override
     public DishViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        Log.i(TAG,"nicholas is going to market to buy a row plank");
+       LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+       View plank = layoutInflater.inflate(R.layout.row_plank,parent,false);
+        return new DishViewHolder(plank);
     }
 
     @Override
     public void onBindViewHolder(@NonNull DishViewHolder holder, int position) {
+        Log.i(TAG,"christian- is writing"+ dishes[position]);
+        holder.dishTextView.setText(dishes[position]);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        Log.i(TAG,"kenneth- counting the no of items in the data");
+
+        return dishes.length;
     }
 
     public class DishViewHolder extends RecyclerView.ViewHolder {
-        public DishViewHolder(@NonNull View itemView) {
-            super(itemView);
+        TextView dishTextView;
+        public DishViewHolder(@NonNull View nicholasPlank) {
+            super(nicholasPlank);
+            Log.i(TAG,"david- vh got the plank");
+            dishTextView = nicholasPlank.findViewById(R.id.tvCuisine);
+
         }
     }
 }
--- app/src/main/java/com/example/cognizantapp/RecyclerActivity.kt
+++ app/src/main/java/com/example/cognizantapp/RecyclerActivity.kt
@@ -2,14 +2,22 @@ package com.example.cognizantapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerActivity : AppCompatActivity() {
     lateinit var menuRecyclerView: RecyclerView
-    var dishes = arrayOf("pizza","burger","ice cream")
+    var dishes = arrayOf("pizza","burger","ice cream",
+        "pizza","burger","ice cream","pizza","burger","ice cream","pizza","burger","ice cream",
+        "pizza","burger","ice cream","pizza","burger","ice cream","pizza","burger","ice cream",
+        "pizza","burger","ice cream","pizza","burger","ice cream","pizza","burger","ice cream",
+        )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler)
         menuRecyclerView = findViewById(R.id.menuRecyclerview)
+        menuRecyclerView.layoutManager = LinearLayoutManager(this)
+        var adapter = DishAdapter(dishes)
+        menuRecyclerView.adapter = adapter
     }
 }
