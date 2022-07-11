37ef70a5458e6622918f8feb332521135f340f9b
abdul ansari <abdul.tanveer@gmail.com>
2022-07-11 19:16:42 +0530

    recyclerview part -2

--- app/src/main/java/com/example/cognizantapp/DishAdapter.java
+++ app/src/main/java/com/example/cognizantapp/DishAdapter.java
@@ -1,7 +1,34 @@
 package com.example.cognizantapp;
 
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 //holder = reserve
+//DishAdapter.DishViewHolder = inner class
+public class DishAdapter extends RecyclerView.Adapter<DishAdapter.DishViewHolder> {
 
-public class DishAdapter extends RecyclerView.Adapter<DishViewHolder> {
+
+    @NonNull
+    @Override
+    public DishViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DishViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class DishViewHolder extends RecyclerView.ViewHolder {
+        public DishViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
