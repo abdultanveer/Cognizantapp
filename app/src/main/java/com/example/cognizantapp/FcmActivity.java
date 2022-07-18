89d83b2f17173ff1f8c1d235d22b3472ef56c8b3
abdul ansari <abdul.tanveer@gmail.com>
2022-07-18 23:50:40 +0530

    shared prefs

--- app/src/main/java/com/example/cognizantapp/FcmActivity.java
+++ app/src/main/java/com/example/cognizantapp/FcmActivity.java
@@ -3,9 +3,11 @@ package com.example.cognizantapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -13,10 +15,12 @@ import com.google.firebase.messaging.FirebaseMessaging;
 
 public class FcmActivity extends AppCompatActivity {
 public static String TAG = FcmActivity.class.getSimpleName();
+    EditText prefsEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fcm);
+        prefsEditText = findViewById(R.id.etPrefs);
     }
 //singleton design pattern
     public void getToken(View view) {
@@ -32,5 +36,38 @@ public static String TAG = FcmActivity.class.getSimpleName();
                     }
                 }
         );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveData();
+    }
+
+    private void saveData() {
+        //get the data from etPrefs
+        String prefs = prefsEditText.getText().toString();
+        //create a file
+        SharedPreferences  preferences = getSharedPreferences("prefs_file_name",MODE_PRIVATE);
+        //open the file in edit mode
+        SharedPreferences.Editor editor = preferences.edit();
+        //write to the file
+        editor.putString("pkey",prefs);
+        //save the file
+        editor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        restoreData();
+    }
+
+    private void restoreData() {
+        //read the data from the file
+        SharedPreferences  preferences = getSharedPreferences("prefs_file_name",MODE_PRIVATE);
+       String prefs = preferences.getString("pkey","");
+        //put it back into the edittext
+        prefsEditText.setText(prefs);
     }
 }
